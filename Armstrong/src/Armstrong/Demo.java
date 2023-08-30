5c8426e69efff5b6a8eed3bb94fb188018c665e3
Seelamanusha <seelamanusha01@gmail.com>
2023-08-30 09:29:09 +0530

    ASSESSMENT PROGRAM

---
+++ Armstrong/src/Armstrong/Demo.java
@@ -0,0 +1,18 @@
+package Armstrong;
+
+public class Demo {
+	int Check(int n) {
+		int sum=0;
+		while(n>0) {
+			int ld=n%10;
+			int cube=ld*3;
+		    sum=sum+cube;
+			n=n/10;
+			
+			
+			
+		}
+		return sum;
+	}
+
+}
